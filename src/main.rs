b60829ef1cde18624bff20e8a8a34e2023eb64d9
Pazzaz <pazzaz.sundqvist@gmail.com>
2018-04-07 14:12:43 +0200

    Change evrything

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 extern crate fnv;
+extern crate indicatif;
 extern crate rayon;
 
 use fnv::FnvHashSet;
-use rayon::prelude::*;
+use indicatif::ProgressBar;
+use indicatif::ProgressStyle;
 use std::collections::HashMap;
-
 const MAP_WIDTH: usize = 3;
 
-struct PositionFinder<'a, 'b> {
+struct PositionFinder<'a, 'b, 'c> {
+    main_positions: &'c [u8],
     output: Vec<u8>,
-    previous_choises: &'b Vec<u8>,
+    previous_choises: &'b [u8],
     tail_length: usize,
     snakes_calculated: &'a HashMap<(u8, usize), FnvHashSet<[bool; MAP_WIDTH * MAP_WIDTH]>>,
     done: bool,
@@ -22,14 +24,16 @@ enum Moves {
     Left,
 }
 
-impl<'a, 'b> PositionFinder<'a, 'b> {
+impl<'a, 'b, 'c> PositionFinder<'a, 'b, 'c> {
     fn new(
-        previous_choises: &'b Vec<u8>,
+        main_positions: &'c [u8],
+        previous_choises: &'b [u8],
         tail_length: usize,
         snakes_calculated: &'a HashMap<(u8, usize), FnvHashSet<[bool; MAP_WIDTH * MAP_WIDTH]>>,
-    ) -> PositionFinder<'a, 'b> {
+    ) -> PositionFinder<'a, 'b, 'c> {
         PositionFinder {
-            output: vec![0],
+            main_positions,
+            output: { vec![0] },
             previous_choises,
             tail_length,
             snakes_calculated: snakes_calculated,
@@ -38,7 +42,7 @@ impl<'a, 'b> PositionFinder<'a, 'b> {
     }
 }
 
-impl<'a, 'b> Iterator for PositionFinder<'a, 'b> {
+impl<'a, 'b, 'c> Iterator for PositionFinder<'a, 'b, 'c> {
     type Item = Vec<u8>;
     fn next(&mut self) -> Option<Self::Item> {
         // We don't need to do anything if we're done
@@ -46,27 +50,30 @@ impl<'a, 'b> Iterator for PositionFinder<'a, 'b> {
             return None;
         }
         loop {
-            // When we've iterated up to MAP_WIDTH*MAP_WIDTH then we've gone through all values
-            // needed for that positions and can increment the previous position
-            if *self.output.last().unwrap() >= (MAP_WIDTH * MAP_WIDTH) as u8 {
+            if self.output.len() == 1 && !self.main_positions.contains(&self.output[0]) {
+                self.output[0] += 1;
+                if self.output[0] == 9 {
+                    self.done = true;
+                    return None;
+                }
+            } else if *self.output.last().unwrap() >= (MAP_WIDTH * MAP_WIDTH) as u8 {
+                // When we've iterated up to MAP_WIDTH*MAP_WIDTH then we've gone through all values
+                // needed for that positions and can increment the previous position
                 self.output.pop();
                 *self.output.last_mut().unwrap() += 1;
                 if self.output[0] == (MAP_WIDTH * MAP_WIDTH) as u8 {
                     self.done = true;
                     return None;
                 }
-
-            // If the last element of our output exists elsewhere in our array,
-            // it's an invalid value and we need to get a new one
             } else if (self.previous_choises.len() == 1
+                // If the last element of our output exists elsewhere in our array,
+                // it's an invalid value and we need to get a new one
                 && self.previous_choises[0] == *self.output.last().unwrap())
                 || {
                     let (last, rest) = self.output.split_last().unwrap();
                     rest.contains(last)
                 } {
                 *self.output.last_mut().unwrap() += 1;
-
-            // Add another backup value if we need it
             } else if self.output.len() < (self.tail_length + 2)
                 && need_backup(
                     &self.previous_choises,
@@ -79,15 +86,15 @@ impl<'a, 'b> Iterator for PositionFinder<'a, 'b> {
                     &self.snakes_calculated,
                     self.tail_length,
                 ) {
+                // Add another backup value if we need it
                 for backup in 0..((MAP_WIDTH * MAP_WIDTH) as u8) {
                     if !self.output.contains(&backup) {
                         self.output.push(backup);
                         break;
                     }
                 }
-
-            // Our output is valid
             } else {
+                // Our output is valid
                 let out = self.output.clone();
                 *self.output.last_mut().unwrap() += 1;
                 if self.output[0] == (MAP_WIDTH * MAP_WIDTH) as u8 {
@@ -128,11 +135,8 @@ fn could_block_all(
             Some(x) => x,
             None => panic!("WRONG"),
         };
-        let mut simple_format = [false; MAP_WIDTH * MAP_WIDTH];
-        for pos in chosen_positions {
-            simple_format[*pos as usize] = true;
-        }
-        if possible_snakes.contains(&simple_format) {
+        let simple = simplify(chosen_positions);
+        if possible_snakes.contains(&simple) {
             return true;
         }
     }
@@ -208,31 +212,34 @@ fn get_valid_snakes(tail_length: usize, head: u8) -> FnvHashSet<[bool; MAP_WIDTH
                 (0, 0) | (2, 2) | (3, 1) => Moves::Left,
                 _ => unreachable!(),
             };
-            last_move = *direction;
             match chosen_move {
                 Moves::Up => {
                     if current_y == 0 {
                         continue 'outer;
                     }
                     current_y -= 1;
+                    last_move = 0;
                 }
                 Moves::Right => {
                     if current_x == MAP_WIDTH as u8 - 1 {
                         continue 'outer;
                     }
                     current_x += 1;
+                    last_move = 1;
                 }
                 Moves::Down => {
                     if current_y == MAP_WIDTH as u8 - 1 {
                         continue 'outer;
                     }
                     current_y += 1;
+                    last_move = 2;
                 }
                 Moves::Left => {
                     if current_x == 0 {
                         continue 'outer;
                     }
                     current_x -= 1;
+                    last_move = 3;
                 }
             }
             let n = current_y * MAP_WIDTH as u8 + current_x;
@@ -303,8 +310,216 @@ fn main() {
         }
     }
     println!("Done generating");
-    let a = PositionFinder::new(&vec![1, 7], 3, &snakes_calculated).count();
-    println!("{}", a);
+    let corners = count_down_tree(1, &[0], &snakes_calculated);
+    println!("{}", corners);
+    let side = count_down_tree(1, &[1], &snakes_calculated);
+    println!("{}", side);
+    let middle = count_down_tree(1, &[4], &snakes_calculated);
+    println!("{}", middle);
+    let final_value: u128 = 4 * corners + 4 * side + middle;
+    println!("{}", final_value);
+}
+
+fn count_down_tree(
+    tail_length: usize,
+    previous_layer: &[u8],
+    snakes_calculated: &HashMap<(u8, usize), FnvHashSet<[bool; MAP_WIDTH * MAP_WIDTH]>>,
+) -> u128 {
+    match symmetricality(simplify(&previous_layer)) {
+        Some(Symmetry::Horizontal) => {
+            let mut possible_tops: Vec<Vec<u8>> = Vec::new();
+            let mut possible_middles: Vec<Vec<u8>> = Vec::new();
+            rayon::join(
+                || {
+                    possible_tops = PositionFinder::new(
+                        &[0, 1, 2],
+                        &previous_layer,
+                        tail_length,
+                        &snakes_calculated,
+                    ).collect();
+                },
+                || {
+                    possible_middles = PositionFinder::new(
+                        &[3, 4, 5],
+                        &previous_layer,
+                        tail_length,
+                        &snakes_calculated,
+                    ).collect();
+                },
+            );
+            let mut top_sum = 0;
+            let mut middle_sum = 0;
+            if tail_length == 6 {
+                top_sum = possible_tops.len() as u128;
+                middle_sum = possible_middles.len() as u128;
+            } else {
+                let bar = ProgressBar::new((possible_tops.len() + possible_middles.len()) as u64);
+                bar.set_style(
+                    ProgressStyle::default_bar()
+                        .template("[{elapsed_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} {msg}"),
+                );
+                bar.set_message(&format!("Horizontal:Top : {}", tail_length));
+                for layer in possible_tops {
+                    bar.inc(1);
+                    top_sum += count_down_tree(tail_length + 1, &layer, snakes_calculated);
+                }
+                bar.set_message(&format!("Horizontal:Middle : {}", tail_length));
+                for layer in possible_middles {
+                    bar.inc(1);
+                    middle_sum += count_down_tree(tail_length + 1, &layer, snakes_calculated);
+                }
+                bar.finish_and_clear();
+            }
+
+            2 * top_sum + middle_sum
+        }
+        Some(Symmetry::Vertical) => {
+            let mut possible_sides: Vec<Vec<u8>> = Vec::new();
+            let mut possible_middles: Vec<Vec<u8>> = Vec::new();
+            rayon::join(
+                || {
+                    possible_sides = PositionFinder::new(
+                        &[0, 3, 6],
+                        &previous_layer,
+                        tail_length,
+                        &snakes_calculated,
+                    ).collect();
+                },
+                || {
+                    possible_middles = PositionFinder::new(
+                        &[1, 4, 7],
+                        &previous_layer,
+                        tail_length,
+                        &snakes_calculated,
+                    ).collect();
+                },
+            );
+            let mut side_sum = 0;
+            let mut middle_sum = 0;
+            if tail_length == 6 {
+                side_sum = possible_sides.len() as u128;
+                middle_sum = possible_middles.len() as u128;
+            } else {
+                let bar = ProgressBar::new((possible_sides.len() + possible_middles.len()) as u64);
+                bar.set_style(
+                    ProgressStyle::default_bar()
+                        .template("[{elapsed_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} {msg}"),
+                );
+                bar.set_message(&format!("Vertical:Side : {}", tail_length));
+                for layer in possible_sides {
+                    bar.inc(1);
+                    side_sum += count_down_tree(tail_length + 1, &layer, snakes_calculated);
+                }
+                bar.set_message(&format!("Vertical:Middle : {}", tail_length));
+                for layer in possible_middles {
+                    bar.inc(1);
+                    middle_sum += count_down_tree(tail_length + 1, &layer, snakes_calculated);
+                }
+                bar.finish_and_clear();
+            }
+
+            2 * side_sum + middle_sum
+        }
+        Some(Symmetry::Full) => {
+            let mut possible_corners: Vec<Vec<u8>> = Vec::new();
+            let mut possible_sides: Vec<Vec<u8>> = Vec::new();
+            rayon::join(
+                || {
+                    possible_corners = PositionFinder::new(
+                        &[0],
+                        &previous_layer,
+                        tail_length,
+                        &snakes_calculated,
+                    ).collect();
+                },
+                || {
+                    possible_sides = PositionFinder::new(
+                        &[1],
+                        &previous_layer,
+                        tail_length,
+                        &snakes_calculated,
+                    ).collect();
+                },
+            );
+            let possible_middles: Vec<Vec<u8>> =
+                PositionFinder::new(&[4], &previous_layer, tail_length, &snakes_calculated)
+                    .collect();
+            let mut corner_sum = 0;
+            let mut side_sum = 0;
+            let mut middle_sum = 0;
+            if tail_length == 6 {
+                corner_sum = possible_corners.len() as u128;
+                side_sum = possible_sides.len() as u128;
+                middle_sum = possible_middles.len() as u128;
+            } else {
+                let bar = ProgressBar::new(
+                    (possible_corners.len() + possible_sides.len() + possible_middles.len()) as u64,
+                );
+                bar.set_style(
+                    ProgressStyle::default_bar()
+                        .template("[{elapsed_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} {msg}"),
+                );
+                bar.set_message(&format!("Full:Corner : {}", tail_length));
+                for layer in possible_corners {
+                    bar.inc(1);
+                    corner_sum += count_down_tree(tail_length + 1, &layer, snakes_calculated);
+                }
+                bar.set_message(&format!("Full:Side : {}", tail_length));
+                for layer in possible_sides {
+                    bar.inc(1);
+                    side_sum += count_down_tree(tail_length + 1, &layer, snakes_calculated);
+                }
+                bar.set_message(&format!("Full:Middle : {}", tail_length));
+                for layer in possible_middles {
+                    bar.inc(1);
+                    middle_sum += count_down_tree(tail_length + 1, &layer, snakes_calculated);
+                }
+                bar.finish_and_clear();
+            }
+            4 * corner_sum + 4 * side_sum + middle_sum
+        }
+        None => {
+            let mut first_half: Vec<Vec<u8>> = Vec::new();
+            let mut second_half: Vec<Vec<u8>> = Vec::new();
+            rayon::join(
+                || {
+                    first_half = PositionFinder::new(
+                        &[4, 5, 6, 7, 8],
+                        &previous_layer,
+                        tail_length,
+                        &snakes_calculated,
+                    ).collect();
+                },
+                || {
+                    second_half = PositionFinder::new(
+                        &[0, 1, 2, 3],
+                        &previous_layer,
+                        tail_length,
+                        &snakes_calculated,
+                    ).collect();
+                },
+            );
+            first_half.append(&mut second_half);
+            let possible_positions = first_half;
+            let mut sum = 0;
+            if tail_length == 6 {
+                sum = possible_positions.len() as u128
+            } else {
+                let bar = ProgressBar::new(possible_positions.len() as u64);
+                bar.set_style(
+                    ProgressStyle::default_bar()
+                        .template("[{elapsed_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} {msg}"),
+                );
+                bar.set_message(&format!("None : {}", tail_length));
+                for layer in possible_positions {
+                    bar.inc(1);
+                    sum += count_down_tree(tail_length + 1, &layer, snakes_calculated);
+                }
+                bar.finish_and_clear();
+            }
+            sum
+        }
+    }
 }
 
 enum Symmetry {
@@ -313,9 +528,18 @@ enum Symmetry {
     Full,
 }
 
-fn is_symmetrical(points: [u8; 9]) -> Option<Symmetry> {
-    let vertical = points[0] == points[6] && points[1] == points[7] && points[2] == points[8];
-    let horizontal = points[0] == points[2] && points[3] == points[5] && points[6] == points[8];
+#[inline]
+fn simplify(points: &[u8]) -> [bool; 9] {
+    let mut simple_format = [false; 9];
+    for pos in points {
+        simple_format[*pos as usize] = true;
+    }
+    simple_format
+}
+
+fn symmetricality(points: [bool; 9]) -> Option<Symmetry> {
+    let horizontal = points[0] == points[6] && points[1] == points[7] && points[2] == points[8];
+    let vertical = points[0] == points[2] && points[3] == points[5] && points[6] == points[8];
     if horizontal && vertical {
         Some(Symmetry::Full)
     } else if vertical {
